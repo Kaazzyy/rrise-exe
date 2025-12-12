5d22e170bcbef7263cd6a3ba869cacdd88289e22
Kaazzyy <maneljoao383@gmail.com>
2025-12-12 23:15:19 +0000

    Update play.js

--- js/play.js
+++ js/play.js
@@ -1,10 +1,9 @@
 document.getElementById("playBtn").onclick = function () {
     const nick = document.getElementById("nickname").value || "Player";
     const skin = document.getElementById("skin").value || "";
-
+    
     localStorage.nickname = nick;
     localStorage.skinUrl = skin;
-
+    
     window.location.href = "https://aetlis.io/";
 };
-
